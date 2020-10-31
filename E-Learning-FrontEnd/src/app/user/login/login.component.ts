6c37641dfd950bc932821d3c07ce188d712a1cc0
aaltwaim <a_t1411@hotmail.com>
2020-10-31 13:38:46 +0300

    integration of login

--- E-Learning-FrontEnd/src/app/user/login/login.component.ts
+++ E-Learning-FrontEnd/src/app/user/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from './user.service';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class LoginComponent implements OnInit {
+  formModel ={
+    UserName: '',
+    Password: ''
+  }
 
-  constructor() { }
+  constructor(private service: UserService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    if (localStorage.getItem('token') != null)
+    this.router.navigateByUrl('/home');
+  }
+  onSubmit(form: NgForm) {
+    this.service.login(form.value).subscribe(
+      (res: any) => {
+        localStorage.setItem('token', res.token);
+        this.router.navigateByUrl('/home');
+      },
+      err => {
+        if (err.status == 400)
+          this.toastr.error('Incorrect username or password.', 'Authentication failed.');
+        else
+          console.log(err);
+      }
+    );
   }
 
 }
